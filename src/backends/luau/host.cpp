3cbb43ac9406300a11fa311c58224e1136e72be0
lammmab <liamlam8282@gmail.com>
2026-06-07 19:11:06 -0400

    Prefer newclass over class (class is reserved in Luau; incompatible with LSP)

--- src/backends/luau/host.cpp
+++ src/backends/luau/host.cpp
@@ -27,7 +27,7 @@
 
 static const struct { const char* name; const unsigned char* src; unsigned int len; } s_prelude_libs[] = {
     { "Augment.Hook",       hook_runtime_luau,  hook_runtime_luau_len },
-    { "Petrichor.Standard", standard_luau,      standard_luau_len     }, // globals; class(), etc.
+    { "Petrichor.Standard", standard_luau,      standard_luau_len     }, // globals; newclass(), etc.
     { "Petrichor.Math",     math_luau,          math_luau_len         },
     { "Petrichor.Json",     json_luau,          json_luau_len         },
     { "Petrichor.Mod",      petrichor_mod_luau, petrichor_mod_luau_len },
