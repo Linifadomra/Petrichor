27fe5ecb2d19303334b351265d0c6c183ee7a8e2
lammmab <liamlam8282@gmail.com>
2026-06-06 21:41:38 -0400

    Make logging const (as it should be)

--- include/petrichor/petrichor.h
+++ include/petrichor/petrichor.h
@@ -42,7 +42,7 @@ struct PetrichorEvents {
 };
 
 struct IPetrichorHost {
-    virtual void        log           (const char* tag, const char* msg) = 0;
+    virtual void        log           (const char* tag, const char* msg) const = 0;
     virtual const char* mods_dir      () const = 0;
     virtual void*       alloc         (uint32_t bytes) = 0;
     virtual void*       resolve       (const char* sym) const = 0;
